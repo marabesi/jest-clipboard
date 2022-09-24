98737a4d7b490f701c1721f2a4278a9356317303
Matheus Marabesi <matheus.marabesi@gmail.com>
2022-09-24 22:45:44 +0200

    chore: preset ts-jest

--- src/clipboard.test.ts
+++ src/clipboard.test.ts
@@ -1,5 +1,23 @@
-describe('clipboard', function () {
-    it('Write to clipboard', () => {
+import {setUpClipboard, tearDownClipboard} from "./clipboard";
 
+const writeToClipboard = async () => {
+    await navigator.clipboard.writeText('text from clipboard');
+};
+
+describe('Clipboard', () => {
+    beforeEach(() => {
+        setUpClipboard('text from clipboard');
+    });
+
+    afterEach(() => {
+        tearDownClipboard();
+    });
+
+    it('write from clipboard', async () => {
+        jest.spyOn(global.navigator.clipboard, 'writeText');
+
+        await writeToClipboard();
+
+        expect(global.navigator.clipboard.writeText).toHaveBeenCalledWith('text from clipboard');
     });
 });
