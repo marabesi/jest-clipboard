af0ba3b3e496068f42817ec8ccf597da91a8fe72
Matheus Marabesi <matheus.marabesi@gmail.com>
2022-09-25 11:01:44 +0200

    chore: set identation to be 2

--- src/clipboard.test.ts
+++ src/clipboard.test.ts
@@ -1,23 +1,23 @@
 import {setUpClipboard, tearDownClipboard} from "./clipboard";
 
 const writeToClipboard = async () => {
-    await navigator.clipboard.writeText('text from clipboard');
+  await navigator.clipboard.writeText('text from clipboard');
 };
 
 describe('Clipboard', () => {
-    beforeEach(() => {
-        setUpClipboard('text from clipboard');
-    });
+  beforeEach(() => {
+    setUpClipboard('text from clipboard');
+  });
 
-    afterEach(() => {
-        tearDownClipboard();
-    });
+  afterEach(() => {
+    tearDownClipboard();
+  });
 
-    it('write from clipboard', async () => {
-        jest.spyOn(global.navigator.clipboard, 'writeText');
+  it('write from clipboard', async () => {
+    jest.spyOn(global.navigator.clipboard, 'writeText');
 
-        await writeToClipboard();
+    await writeToClipboard();
 
-        expect(global.navigator.clipboard.writeText).toHaveBeenCalledWith('text from clipboard');
-    });
+    expect(global.navigator.clipboard.writeText).toHaveBeenCalledWith('text from clipboard');
+  });
 });
